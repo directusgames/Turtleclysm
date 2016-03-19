45abdf090d53e8f3b38aa6dfb80d285a304075fd
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2016-03-19 13:58:30 +0800

    Basic keyboard input.

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public int m_size; // How fat our boy franklinson gets.
+	public int m_depth; // Value of how deep in the oceanz.
+
+	// Use this for initialization
+	void Start () {
+		m_size = 1;
+		m_depth = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+	public Player m_player;
+	public float m_speed;
+	public Transform m_playerPos;
+	public Rigidbody2D m_rigidBody;
+
+	// Use this for initialization
+	void Start () {
+		m_speed = 5f;
+	}
+	
+	// Update is called once prior to each 'physics step'.
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		m_rigidBody.velocity = new Vector3(
+			moveHorizontal * m_speed,
+			moveVertical * m_speed,
+			0f
+		);
+
+		// if (Input.GetKeyDown(KeyCode.W)) {
+		// }
+	}
+}
