f0c4a82e033ef3bfac7f89a522cb11e4b6d51c54
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2016-03-20 13:25:55 +0800

    NPC spawning at random points in corners done (again).

--- Assets/Scripts/NPCGenerator.cs
+++ Assets/Scripts/NPCGenerator.cs
@@ -6,16 +6,43 @@ public class NPCGenerator : MonoBehaviour {
 
 	public List<GameObject> m_npcList;
 
+	public bool m_spawn = true;
+
+	public int m_spawnPosItr;
+	public Vector3[] m_spawnPositions;
+
 	// Use this for initialization
 	void Start () {
 		if (m_npcList.Count == 0) {
 			populatePrefabs();
 		}
+
+		m_spawnPosItr = 0;
+		m_spawnPositions = new Vector3[] {
+			new Vector3(11f, 7f, -1f),
+			new Vector3(11f, -7f, -1f),
+			new Vector3(-11f, 7f, -1f),
+			new Vector3(-11f, -7f, -1f)
+		};
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (m_spawn) {
+			for (int itr = 0; itr < m_npcList.Count; ++itr) {
+				GameObject go = (GameObject) Instantiate(
+					m_npcList[itr],
+					m_spawnPositions[m_spawnPosItr],
+					Quaternion.identity
+				);
+				if (m_spawnPosItr < 3) {
+					++m_spawnPosItr;
+				} else {
+					m_spawnPosItr = 0;
+				}
+			}
+			m_spawn = false;
+		}
 	}
 
 	void populatePrefabs() {
