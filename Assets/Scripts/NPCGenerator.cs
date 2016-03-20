f7ec9202a81beaf6d339153cb5d1ec4a66384933
Michael J. Kiernan <mikeyjkiernan@gmail.com>
2016-03-20 12:11:43 +0800

    Moved NPC prefabs to Resources/Prefabs/NPCs/. Created helper method for populating prefab NPC list.

---
+++ Assets/Scripts/NPCGenerator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class NPCGenerator : MonoBehaviour {
+
+	public List<GameObject> m_npcList;
+
+	// Use this for initialization
+	void Start () {
+		if (m_npcList.Count == 0 {
+			populatePrefabs();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void populatePrefabs() {
+		m_npcList = new List<GameObject>();
+		Object[] temp = Resources.LoadAll("Prefabs/NPCs"); 
+		for (int itr = 0; itr < temp.Length; ++itr) {
+			m_npcList.Add((GameObject) temp.GetValue(itr));
+		}
+	}
+}
