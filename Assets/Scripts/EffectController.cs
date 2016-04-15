b297ea604cbf875d694fd74de5e65d690d6c7616
directusgames <directusgames@gmail.com>
2016-04-16 00:51:33 +0800

    Added lifetime for death effect
    Added scaling of NPC when spawned

---
+++ Assets/Scripts/EffectController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectController : MonoBehaviour {
+
+    public float lifetime;
+    
+    private float curTime;
+
+	// Use this for initialization
+	void Start () {
+    
+        curTime = 0;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    
+        curTime += Time.deltaTime;
+        if(curTime >= lifetime)
+        {
+            Destroy (this.gameObject);
+        }
+	
+	}
+}
--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -4,12 +4,16 @@ using System.Collections;
 public class NPCController : MonoBehaviour {
     
     //sizeAddition is the amount of size to add to the turtle when eaten
-    public float speed, minSize, maxSize, size, sizeAddition;
+    public float speed, minSize, maxSize, size, sizeAddition, minDepth, maxDepth;
     public GameObject deathEffect;    
     private Rigidbody2D rigid;
       
 	// Use this for initialization
 	void Start () {
+    
+        size = Random.Range (minSize,maxSize);
+        
+        transform.localScale = new Vector3(size,size,1f);
         
         rigid = GetComponent<Rigidbody2D>();
         rigid.velocity = transform.up * speed; 
